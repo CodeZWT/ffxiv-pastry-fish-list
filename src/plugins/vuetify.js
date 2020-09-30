ea1973b844266a3f714b4e2fbd1e675fa335414f
etenaelta <etenaelta@gmail.com>
2020-09-30 23:00:39 +0800

    update to use material rally preset to set the theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,20 +1,22 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import colors from 'vuetify/lib/util/colors'
+// import colors from 'vuetify/lib/util/colors'
+import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  preset,
   theme: {
-    dark: true,
+    // dark: true,
     mobileBreakpoint: 960,
-    themes: {
-      dark: {
-        primary: colors.deepPurple.base,
-        success: colors.teal.darken4,
-        info: colors.indigo.darken4,
-        secondary: colors.blueGrey.darken4
-      },
-    },
+    // themes: {
+    //   dark: {
+    //     primary: colors.deepPurple.base,
+    //     success: colors.teal.darken4,
+    //     info: colors.indigo.darken4,
+    //     secondary: colors.blueGrey.darken4,
+    //   },
+    // },
   },
 })
