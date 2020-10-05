4d8b0eba0913fc22cd2cb9dc43672fe160355389
etenaelta <etenaelta@gmail.com>
2020-10-05 20:52:55 +0800

    custom colors to darken-1

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,17 +8,19 @@ Vue.use(Vuetify)
 export default new Vuetify({
   preset,
   breakpoint: {
-    mobileBreakpoint: 'xs' // This is equivalent to a value of 600
+    mobileBreakpoint: 'xs', // This is equivalent to a value of 600
   },
   theme: {
     // dark: true,
-    // themes: {
-    //   dark: {
-    //     primary: colors.deepPurple.base,
-    //     success: colors.teal.darken4,
-    //     info: colors.indigo.darken4,
-    //     secondary: colors.blueGrey.darken4,
-    //   },
-    // },
+    themes: {
+      dark: {
+        primary: '#009d67',
+        secondary: '#00453f',
+        tertiary: '#df4c41',
+        quaternary: '#e0b425',
+        quinary: '#9442e2',
+        senary: '#52c2e2',
+      },
+    },
   },
 })
