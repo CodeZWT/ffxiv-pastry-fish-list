2f3a3997848c10de534909240e9b4aac21c57fad
RiceCake404 <etenaelta@outlook.com>
2020-12-11 20:54:31 +0800

    show migration source select dialog if both user data existed

--- src/views/MigrationPage.vue
+++ src/views/MigrationPage.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="fill-height">
-    <v-overlay :value="migrating" z-index="9999">
+    <v-overlay :value="migrationStep === 'migrating'" z-index="9999">
       <v-progress-circular :size="70" :width="7" indeterminate></v-progress-circular>
       <div class="subtitle-1 mt-2">数据迁移中</div>
     </v-overlay>
-    <v-card v-if="!migrating" class="fill-height">
+    <v-card v-if="migrationStep === 'finished'" class="fill-height">
       <v-card-text class="fill-height d-flex align-center text-h6 justify-center">
         <div v-if="migrateSuccess" class="d-flex align-center">
           <v-icon color="primary" x-large class="mr-1">mdi-database-check</v-icon>
           <div>
             数据迁移成功！<br />
-            即将跳转至时钟首页。
+            即将跳转至鱼糕首页。
           </div>
         </div>
         <div v-else class="d-flex align-center">
@@ -23,6 +23,26 @@
         </div>
       </v-card-text>
     </v-card>
+    <v-dialog
+      :value="showDataSelection && !loading"
+      @input="showDataSelection = false"
+      persistent
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          检测到同时存在正式版和测试版数据，请选择导入的数据来源。
+        </v-card-title>
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="primary" @click="migrateFromProd">
+            从正式版导入
+          </v-btn>
+          <v-btn color="info" @click="migrateFromTest">
+            从测试版导入
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <iframe
       v-show="false"
       src="https://ricecake404.gitee.io/ff14-list-test/migration.html"
@@ -33,11 +53,21 @@
 
 <script>
 import DataUtil from '@/utils/DataUtil'
-import { mapMutations } from 'vuex'
+import { mapMutations, mapState } from 'vuex'
 
 export default {
   name: 'MigrationPage.vue',
-  data: () => ({ migrating: true, migrateSuccess: undefined }),
+  data: () => ({
+    migrationStep: 'waiting',
+    migrateSuccess: undefined,
+    showDataSelection: false,
+    dataToBeMigrated: undefined,
+    data: undefined,
+    testData: undefined,
+  }),
+  computed: {
+    ...mapState(['loading']),
+  },
   mounted() {
     window.addEventListener('message', this.receiveUserData, false)
     const iFrame = document.getElementById('migration-src')
@@ -47,6 +77,7 @@ export default {
   },
   methods: {
     importData(dataStr) {
+      this.migrationStep = 'migrating'
       if (dataStr == null) {
         console.log('no data to migrate')
         this.endMigration()
@@ -66,17 +97,43 @@ export default {
       }
     },
     endMigration() {
-      this.migrating = false
+      this.migrationStep = 'finished'
       this.migrateSuccess = true
       setTimeout(() => {
         this.$router.push({ name: 'ListPage' })
       }, 3000)
     },
     receiveUserData(event) {
+      console.log(event)
       if (event?.data?.type === 'migration-data') {
         console.log('receiveUserData', event?.data)
-        this.importData(event?.data?.data)
+        this.data = event?.data?.data
+        this.testData = event?.data?.testData
+        if (this.data && this.testData) {
+          this.showDataSelection = true
+        } else if (this.data && !this.testData) {
+          this.migrateFromProd()
+          // this.importData(data)
+        } else if (!this.data && this.testData) {
+          this.migrateFromTest()
+          // this.importData(testData)
+        } else {
+          // actually do nothing
+          this.migrate()
+        }
       }
+    },
+    migrateFromProd() {
+      this.dataToBeMigrated = this.data
+      this.migrate()
+    },
+    migrateFromTest() {
+      this.dataToBeMigrated = this.testData
+      this.migrate()
+    },
+    migrate() {
+      this.showDataSelection = false
+      this.importData(this.dataToBeMigrated)
     },
     showInfo(text, color) {
       this.showSnackbar({
