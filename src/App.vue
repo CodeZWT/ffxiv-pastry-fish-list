027e27e46520e776e5bc98769fcb6744e1456125
RiceCake404 <etenaelta@outlook.com>
2020-12-11 21:40:14 +0800

    add migration dialog

--- src/App.vue
+++ src/App.vue
@@ -302,6 +302,36 @@
 
     <fish-setting-dialog v-model="showSettingDialog" />
 
+    <v-dialog v-model="showMigrationDialog" max-width="600px" persistent>
+      <v-card>
+        <v-card-title>
+          网站迁移公告
+        </v-card-title>
+        <v-card-text>
+          <div class="text-h6">
+            为了提供更稳定的服务，鱼糕已迁移至新站点。点击下方按钮，将跳转并同时将数据迁移至新站。
+          </div>
+          <p />
+          <div>数据迁移后，旧站点的数据还会保留。</div>
+          <div>旧站点页面也会保留一段时间，但是不会有新的功能更新。</div>
+          <div class="mt-2">
+            <v-btn color="primary" @click="goMigration" block x-large>
+              迁移数据并前往新站点
+            </v-btn>
+          </div>
+        </v-card-text>
+        <v-card-text>
+          <v-btn
+            block
+            @click="showMigrationDialog = false"
+            :disabled="migrationCancelCountdown > 0"
+          >
+            放弃({{ migrationCancelCountdown }})
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
     <v-dialog v-model="showPatchNoteDialog" max-width="600px" scrollable>
       <v-card>
         <v-card-title>
@@ -313,7 +343,11 @@
           <ul>
             <li>
               增加攻略支持：（感谢攻略作者的授权与支持！）
-              <div>在详情界面或直接点击<v-icon small>mdi-book</v-icon>按钮即可查看。</div>
+              <div>
+                在详情界面或直接点击
+                <v-icon small>mdi-book</v-icon>
+                按钮即可查看。
+              </div>
               <ul>
                 <li>
                   <a
@@ -734,6 +768,7 @@ export default {
     showAboutDialog: false,
     showSettingDialog: false,
     showPatchNoteDialog: false,
+    showMigrationDialog: true,
     collapse: false,
     // listFishCnt: [{ cnt: 0 }, { cnt: 0 }, { cnt: 0 }],
     TABS: DataUtil.TABS,
@@ -756,6 +791,7 @@ export default {
     loading: true,
     extraFishListTimePart: {},
     lazyFishWindowRates: {},
+    migrationCancelCountdown: 20,
   }),
   computed: {
     // TODO: CHECK different with real eorzea time of 1 minute
@@ -1036,12 +1072,12 @@ export default {
     this.loading = true
     this.drawer = !this.isMobile
     this.$vuetify.theme.dark = this.dark
-    if (
-      this.toComparableVersion(this.version) >
-      this.toComparableVersion(this.websiteVersion)
-    ) {
-      this.showPatchNoteDialog = true
-    }
+    // if (
+    //   this.toComparableVersion(this.version) >
+    //   this.toComparableVersion(this.websiteVersion)
+    // ) {
+    //   this.showPatchNoteDialog = true
+    // }
 
     hotkeys('/', event => {
       if (!this.showSearchDialog) {
@@ -1052,6 +1088,13 @@ export default {
 
     const path = window.location.pathname.replaceAll('/', '')
     countapi.hit(window.location.host.replace(':', ''), path === '' ? 'root' : path)
+
+    const timerId = setInterval(() => {
+      this.migrationCancelCountdown--
+      if (this.migrationCancelCountdown <= 0) {
+        clearInterval(timerId)
+      }
+    }, 1000)
   },
   async mounted() {
     this.cafeKitTooltipCopyPatch()
@@ -1401,6 +1444,9 @@ export default {
     },
     onFishIconClicked() {
       this.toggleCollapse()
+    },
+    goMigration() {
+      window.location.href = 'https://fish.ricecake302.com/#/migration'
     },
     ...mapMutations([
       'toggleFilterPanel',
