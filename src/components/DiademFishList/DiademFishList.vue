02fefe38a17ec2840c4e47095bc26b3b7650b9f0
RiceCake404 <etenaelta@outlook.com>
2020-12-27 13:43:49 +0800

    move diadem fish list to standalone component

---
+++ src/components/DiademFishList/DiademFishList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <component :is="component" :spot-data="spotData" />
+  </div>
+</template>
+
+<script>
+import DiademFishListLarge from '@/components/DiademFishList/DiademFishListLarge'
+import DiademFishListSmall from '@/components/DiademFishList/DiademFishListSmall'
+
+export default {
+  name: 'DiademFishList',
+  components: { DiademFishListLarge, DiademFishListSmall },
+  props: ['spotData'],
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.mobile
+    },
+    component() {
+      const mode = this.isMobile ? 'small' : 'large'
+      switch (mode) {
+        case 'large':
+          return 'DiademFishListLarge'
+        case 'small':
+          return 'DiademFishListSmall'
+        default:
+          return 'FishInfoRowLarge'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
