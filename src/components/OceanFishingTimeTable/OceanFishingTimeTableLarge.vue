7502204fbb7ae6190d3dd8d5d2af44b9e87479cf
RiceCake404 <etenaelta@outlook.com>
2020-12-31 11:34:42 +0800

    give more space to voyage detail column

--- src/components/OceanFishingTimeTable/OceanFishingTimeTableLarge.vue
+++ src/components/OceanFishingTimeTable/OceanFishingTimeTableLarge.vue
@@ -74,8 +74,8 @@
             <colgroup>
               <col span="1" style="width: 15%;" />
               <col span="1" style="width: 20%;" />
-              <col span="1" style="width: 25%;" />
-              <col span="1" style="width: 40%;" />
+              <col span="1" style="width: 20%;" />
+              <col span="1" style="width: 45%;" />
             </colgroup>
             <thead>
               <tr>
