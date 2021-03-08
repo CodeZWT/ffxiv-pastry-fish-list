e750e1d597289d9e195cd738d798ddf74e99d776
RiceCake404 <etenaelta@outlook.com>
2021-03-08 20:38:02 +0800

    fix indexeddb version

--- front/src/plugins/db.js
+++ front/src/plugins/db.js
@@ -4,6 +4,9 @@ const db = new Dexie('pastry-fish-db')
 db.version(1).stores({
   records:
     'id,startTime,biteTime,tug,hookset,fishId,hq,size,spotId,snagging,chum,fishEyes,surfaceScale,identicalCast,baitId,mooch',
+})
+
+db.version(2).stores({
   sounds: 'id,base64,filePath',
 })
 
