f07ee348bd1c84fab89e81c258a789130423fafa
RiceCake404 <etenaelta@outlook.com>
2021-06-02 00:15:22 +0800

    fix update time

--- front/src/components/Dialog/PatchNoteDialog.vue
+++ front/src/components/Dialog/PatchNoteDialog.vue
@@ -25,7 +25,7 @@
         <!--          更新国服5.41后，渔捞与同步功能不可用，请耐心等待自动更新。-->
         <!--        </v-alert>-->
         <div class="text-h6">Version 0.8.6</div>
-        <div class="text-subtitle-1">更新时间: 2021/06/01 00:00</div>
+        <div class="text-subtitle-1">更新时间: 2021/06/02 00:00</div>
         <div class="text-h5 text-center my-1">鱼糕</div>
         <ul>
           <li>
