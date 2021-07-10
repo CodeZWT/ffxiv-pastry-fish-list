ffe0c7f73e906bdeab4244088c75b5f11b8214f9
RiceCake404 <etenaelta@outlook.com>
2021-07-11 00:25:43 +0800

    fix(desktop): fix switch mini-mode timer

--- main.js
+++ main.js
@@ -68,38 +68,38 @@ const DEFAULT_HOTKEY_SETTING = {
 }
 const DEFAULT_WINDOW_SETTING = {
   main: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 1080, h: 768 },
     opacity: 0.95,
     zoomFactor: 1,
   },
   setting: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 500, h: 500 },
     opacity: 0.95,
     zoomFactor: 1,
   },
   timer: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 500, h: 250 },
     opacity: 0.95,
     zoomFactor: 1,
   },
   timerMini: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 500, h: 120 },
     opacity: 0.95,
     zoomFactor: 1,
     enabled: false,
   },
   history: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 500, h: 800 },
     opacity: 0.95,
     zoomFactor: 1,
   },
   spotStatistics: {
-    pos: { x: null, y: null },
+    pos: { x: 100, y: 100 },
     size: { w: 500, h: 500 },
     opacity: 0.95,
     zoomFactor: 1,
