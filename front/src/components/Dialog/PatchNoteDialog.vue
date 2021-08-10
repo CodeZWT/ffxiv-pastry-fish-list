65dc57c540421f4db62641445ce3808ef2c3c4b8
RiceCake404 <etenaelta@outlook.com>
2021-08-11 00:02:44 +0800

    chore(version): update to version 0.9.2

--- front/src/components/Dialog/PatchNoteDialog.vue
+++ front/src/components/Dialog/PatchNoteDialog.vue
@@ -21,9 +21,17 @@
       <v-divider />
       <v-card-text style="max-height: 600px">
         <!-- patch update wait note -->
-        <v-alert outlined type="warning" border="left">
-          更新国服5.5后，渔捞与同步功能不可用，请耐心等待自动更新。
-        </v-alert>
+        <!--        <v-alert outlined type="warning" border="left">-->
+        <!--          更新国服5.5后，渔捞与同步功能不可用，请耐心等待自动更新。-->
+        <!--        </v-alert>-->
+        <div class="text-h6">Version 0.9.2</div>
+        <div class="text-subtitle-1">更新时间: 2021/08/10 23:50</div>
+        <div class="text-h5 text-center my-1">鱼糕桌面版</div>
+        <ul>
+          <li>支持国服5.5</li>
+        </ul>
+
+        <v-divider />
 
         <div class="text-h6">Version 0.9.1</div>
         <div class="text-subtitle-1">更新时间: 2021/08/09 22:50</div>
