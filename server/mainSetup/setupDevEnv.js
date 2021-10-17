a229d427a9af5e419bc33bc3b83eb0b6f75d4891
RiceCake404 <etenaelta@outlook.com>
2021-10-17 17:43:29 +0800

    fix(dev): remove vue dev tool install

--- server/mainSetup/setupDevEnv.js
+++ server/mainSetup/setupDevEnv.js
@@ -3,13 +3,13 @@ const log = require('electron-log')
 
 const setupDevEnv = () => {
   if (isDev) {
-    const {
-      default: installExtension,
-      VUEJS_DEVTOOLS,
-    } = require('electron-devtools-installer')
-    installExtension(VUEJS_DEVTOOLS)
-      .then(name => log.info(`Added Extension:  ${name}`))
-      .catch(err => log.info('An error occurred: ', err))
+  //   const {
+  //     default: installExtension,
+  //     VUEJS_DEVTOOLS,
+  //   } = require('electron-devtools-installer')
+  //   installExtension(VUEJS_DEVTOOLS)
+  //     .then(name => log.info(`Added Extension:  ${name}`))
+  //     .catch(err => log.info('An error occurred: ', err))
   }
 }
 
