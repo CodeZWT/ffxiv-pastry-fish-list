d7d2fa49444a7e31244a43747b0a7f295e28091d
RiceCake404 <etenaelta@outlook.com>
2021-10-25 01:21:34 +0800

    chore(patch): update patch to 1.0.0

--- front/src/components/Dialog/PatchNoteDialog.vue
+++ front/src/components/Dialog/PatchNoteDialog.vue
@@ -24,17 +24,45 @@
         <!--        <v-alert outlined type="warning" border="left">-->
         <!--          更新国服5.55后，渔捞与同步功能不可用，请耐心等待自动更新。-->
         <!--        </v-alert>-->
-        <v-subheader> 仅在网页版随时更新，改动将记入下一版本。</v-subheader>
+        <!--        <v-subheader> 仅在网页版随时更新，改动将记入下一版本。</v-subheader>-->
         <v-alert color="secondary" outlined class="mt-4">
           <div class="d-flex align-center">
-            <div class="text-h6">Version 0.9.9-fix</div>
+            <div class="text-h6">Version 1.0.0</div>
             <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/10/04 21:21</div>
+            <div class="text-subtitle-1">更新时间: 2021/10/25 01:20</div>
           </div>
         </v-alert>
         <div class="text-h6 my-1">鱼糕</div>
         <ul>
           <li>更新海外访问网页版的支持</li>
+        </ul>
+        <div class="text-h6 my-1">鱼糕桌面版</div>
+        <ul>
+          <li>
+            现所有窗口支持与游戏同步操作。如：可以在鼠标点击窗口的同时操作游戏内角色。
+          </li>
+          <li>
+            增加功能菜单，主窗口内容切换、所有渔捞窗口以及设置窗口均由此访问。
+          </li>
+          <li>
+            <div>
+              <div class="d-inline">
+                <span>增加功能菜单可以通过鼠标点击渔捞图标</span>
+                <img
+                  style="height: 24px; width: 24px"
+                  src="https://cdn.jsdelivr.net/gh/ricecake404/images@main/img/pastry-fish.png"
+                /><span>或使用快捷键访问。（具体请查看键位设置）</span>
+              </div>
+            </div>
+          </li>
+          <li>
+            渔捞界面现与主界面深浅色模式配置同步
+          </li>
+          <li>
+            {{
+              `鱼糕默认显示在主屏幕，多屏幕用户可在“${$t('top.screen')}”中配置显示的屏幕`
+            }}
+          </li>
         </ul>
 
         <v-alert color="secondary" outlined class="mt-4">
