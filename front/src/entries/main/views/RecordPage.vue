0d1f85967ab746ab064241002d7ac1d5b4ec64b5
RiceCake404 <etenaelta@outlook.com>
2021-12-19 19:30:24 +0800

    fix(fish): update region filter

--- front/src/entries/main/views/RecordPage.vue
+++ front/src/entries/main/views/RecordPage.vue
@@ -873,7 +873,7 @@ export default {
         }),
         'region'
       ),
-      regionFilter: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      regionFilter: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     }
   },
   watch: {
