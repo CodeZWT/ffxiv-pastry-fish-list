d5f415294e65a8725c4bdbb36bce29cd3b62a4e7
RiceCake404 <etenaelta@outlook.com>
2022-02-22 01:01:04 +0800

    feat(fish-description): update description

--- front/src/components/fish-detail-items/DetailItemFishDescription.vue
+++ front/src/components/fish-detail-items/DetailItemFishDescription.vue
@@ -12,7 +12,30 @@
         </div>
       </v-expansion-panel-header>
       <v-expansion-panel-content>
-        {{ description }}
+        <v-skeleton-loader
+          v-if="loading"
+          type="list-item-three-line, divider, list-item-three-line"
+        />
+        <template v-else>
+          <div class="pb-1">
+            <div class="subtitle-2 text--disabled d-flex">
+              {{ $t('detail.description.item') }}
+              <v-spacer />
+              {{ $t('detail.description.ilvl') }}{{ ilvl }}
+            </div>
+            <div v-html="description"></div>
+          </div>
+          <v-divider />
+          <div class="pt-1">
+            <div class="subtitle-2 text--disabled">
+              {{ $t('detail.description.fishGuide') }}
+            </div>
+            <div v-text="fishGuide"></div>
+          </div>
+          <div class="d-flex justify-end mt-2">
+            <a :href="link" target="_blank"> Garland数据</a>
+          </div>
+        </template>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
@@ -20,8 +43,9 @@
 
 <script>
 import { mdiBookshelf } from '@mdi/js'
+import DataUtil from '@/utils/DataUtil'
 import EnvMixin from '@/components/basic/EnvMixin'
-import garlandService from '@/service/garlandService'
+import garlandService, { toMultiLangDescription } from '@/service/garlandService'
 
 export default {
   name: 'DetailItemFishDescription',
@@ -39,13 +63,38 @@ export default {
   data: vm => ({
     mdiBookshelf,
     lazyExpansionValue: vm.expanded ? 0 : undefined,
-    description: 'test',
+    description: '',
+    fishGuide: '',
+    ilvl: '',
+    link: '',
+    loading: true,
   }),
-  async mounted() {
-    const fish = garlandService.getItem(this.fish._id)
-    this.description = JSON.stringify(fish)
+  mounted() {},
+  watch: {
+    'fish._id': {
+      handler: async function(id) {
+        try {
+          this.loading = true
+          const resp = await garlandService.getItem(id)
+          this.description = DataUtil.getName(toMultiLangDescription(resp))
+          this.fishGuide = resp?.item?.fish?.guide ?? ''
+          this.ilvl = resp?.item?.ilvl ?? ''
+          this.link = id ? DataUtil.toGarlandItemLink({ id }) : ''
+          this.loading = false
+        } catch (e) {
+          this.description = ''
+          this.fishGuide = ''
+          this.ilvl = ''
+          this.link = ''
+        }
+      },
+      immediate: true,
+    },
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="sass">
+.emphasis
+  font-weight: bold
+</style>
--- front/src/service/garlandService.js
+++ front/src/service/garlandService.js
@@ -1,6 +1,10 @@
 const host = 'https://garlandtools.cn/api/get.php'
 export default {
-  getItem(itemId) {
+  async getItem(itemId) {
+    if (!itemId) {
+      return undefined
+    }
+
     return fetch(host + `?id=${itemId}&type=item&lang=chs&version=3`, {
       headers: {
         'content-type': 'application/json',
@@ -8,4 +12,12 @@ export default {
       method: 'GET',
     }).then(response => response.json())
   },
+}
+
+export const toMultiLangDescription = ret => {
+  return {
+    name_chs: ret?.item?.description ?? '',
+    name_en: ret?.item?.en?.description ?? '',
+    name_ja: ret?.item?.ja?.description ?? '',
+  }
 }
