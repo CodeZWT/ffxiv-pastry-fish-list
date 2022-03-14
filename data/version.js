47713c3803a9dd0f6e6b30dcc7236b587b4e2ba8
RiceCake404 <etenaelta@outlook.com>
2022-03-14 21:19:04 +0800

    fix(filter): fix CN filter

--- data/version.js
+++ data/version.js
@@ -25,10 +25,10 @@ const PATCHES_CN = {
   '3.x': [3.0, 3.1, 3.2, 3.3, 3.4, 3.5],
   '4.x': [4.0, 4.1, 4.2, 4.3, 4.4, 4.5],
   '5.x': [5.0, 5.1, 5.2, 5.3, 5.4, 5.5],
+  '6.x': [6.0],
 }
 
 const PATCHES_GLOBAL = Object.assign({}, PATCHES_CN, {
-  '6.x': [6.0],
 })
 
 const SystemInfo = {
