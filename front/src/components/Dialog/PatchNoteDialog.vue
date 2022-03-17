a04a250769aeb39ff616158d23995658e3e27250
RiceCake404 <etenaelta@outlook.com>
2022-03-17 15:50:02 +0800

    docs: update patch note

--- front/src/components/Dialog/PatchNoteDialog.vue
+++ front/src/components/Dialog/PatchNoteDialog.vue
@@ -27,6 +27,19 @@
         <!--        <v-subheader> 仅在网页版随时更新，改动将记入下一版本。</v-subheader>-->
         <v-alert color="secondary" outlined class="mt-4">
           <div class="d-flex align-center">
+            <div class="text-h6">Version 1.2.0-fix</div>
+            <v-spacer />
+            <div class="text-subtitle-1">更新时间: 2022/03/17 10:00</div>
+          </div>
+        </v-alert>
+        <div class="text-h6 my-1">鱼糕</div>
+        <ul>
+          <li>更新鱼眼模式范围</li>
+        </ul>
+        <v-divider />
+
+        <v-alert color="secondary" outlined class="mt-4">
+          <div class="d-flex align-center">
             <div class="text-h6">Version 1.2.0</div>
             <v-spacer />
             <div class="text-subtitle-1">更新时间: 2022/03/15 10:00</div>
@@ -41,270 +54,6 @@
           <li>增加通知系统，每次启动鱼糕时若有新通知将会弹窗显示。</li>
         </ul>
         <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.10</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2022/03/06 16:50</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕桌面版</div>
-        <ul>
-          <li>修复托盘图标</li>
-          <li>修复页面图标</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.9</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2022/03/06 13:37</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕桌面版</div>
-        <ul>
-          <li>尝试修复下载问题</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.8</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2022/03/05 11:10</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕桌面版</div>
-        <ul>
-          <li>尝试修复下载问题</li>
-          <li>
-            服务商自3/4日起添加了每日总下载量的限制，下载人数过多后会下载失败，若下载失败请尝试手动下载。手动下载地址与自动下载不同，因此不会受到下载量的限制。
-          </li>
-          <li>
-            缩小了安装文件的大小。
-          </li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.7</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2022/03/04 16:10</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>鱼详情中增加资料功能</li>
-          <li>修复刺鱼鱼识及鱼影条件在搜索与笔记页面没有正常显示的问题</li>
-          <li>增加钓场多语言支持</li>
-          <li>增加多语言备选语言支持，当仍未有新版本翻译时，可设置备用显示的语言</li>
-          <li>鱼以及钓场的部分文本更新字体</li>
-          <li>修复导出的一些问题</li>
-          <li>更新贺图</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.6</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/02/06 20:27</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>修复钓鱼笔记页面统计数据无法显示的问题</li>
-          <li>修复历史数据中不包含未知鱼和脱钩鱼记录的问题</li>
-          <li>修复刺鱼跳转钓鱼笔记显示空白的问题</li>
-          <li>钓鱼笔记支持在候选项较少时（1~5）展开搜索结果</li>
-        </ul>
-        <div class="text-h6 my-1">鱼糕桌面版</div>
-        <ul>
-          <li>
-            支持关闭硬件加速（可以帮助解决一部分由于游戏未锁帧导致的显示问题，应用设置后请重启鱼糕）
-          </li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.5</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/1/16 12:20</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>
-            更新部分鱼数据
-            <ul>
-              <li>乌兹刀背鱼·天极：0 - 3:20 修正为 1 - 3:20</li>
-              <li>翻车鱼修正为全天可钓</li>
-              <li>增加部分普通鱼的提钩类型数据</li>
-            </ul>
-          </li>
-        </ul>
-        <div class="text-h6 my-1">鱼糕桌面版</div>
-        <ul>
-          <li>更新新年&降神节贺图，感谢 马猴烧鱼@拉诺西亚 绘制的贺图！</li>
-          <li>修复导出文件空白的问题</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.4</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/1/9 18:20</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>增加维修手册链接（主页及侧边栏）</li>
-          <li>更新钓鱼笔记，在窗口较大时在右侧显示鱼的详情</li>
-          <li>
-            更新钓鱼笔记跳转支持，现在可以选择跳转鱼糕笔记内的鱼页面。同时支持从其他网站跳转的链接。
-          </li>
-          <li>更新水族馆6.0鱼</li>
-          <li>钓鱼笔记支持浏览器的前进后退以及刷新功能。</li>
-          <li>修复统计数据部分钓场重复计算的问题。</li>
-          <li>修复统计数据未正确统计星芒节天气数据的问题。</li>
-          <li>修复统计数据未正确显示6.0钓场新天气的问题。</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.3</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/12/25 23:00</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>更新星芒节贺图.感谢 马猴烧鱼@拉诺西亚 绘制的贺图！</li>
-          <li>更新6.0钓场范围提示图，感谢 轩辕十四 @ 沃仙曦染 测量绘制的范围图！</li>
-          <li>更新台支持，感谢 美依礼芽@延夏 的大力协助！</li>
-          <li>修复手机网页版的部分显示问题</li>
-        </ul>
-        <div class="text-h6 my-1">渔捞</div>
-        <ul>
-          <li>更新国际服6.01支持</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.2</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/12/19 20:00</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>修复饿猫链接。</li>
-          <li>修复6.0钓场统计数据。</li>
-          <li>更新部分鱼数据。</li>
-          <li>
-            增加多语言支持，支持鼠标悬停界面复制中日英的鱼以及鱼饵文本，也可以在左上角切换默认显示语言。
-          </li>
-          <li>
-            更新国际服、国服星芒节支持，根据区服切换的设置运用对应的节日日期进行计算。
-          </li>
-          <li>首页增加微博区域。</li>
-        </ul>
-
-        <v-alert border="top" colored-border type="info" class="mt-4">
-          <div>
-            星芒节期间三主城固定小雪，因此一些鱼的窗口期会受影响：<br />
-            星芒节期间全勤奖普通鱼：求雨鱼<br />
-            星芒节期间窗口期变长：招雨王<br />
-            星芒节期间无窗口期：倔强鲫鱼、抽须王、枪鼻头、芳香蝾螈 (<s>我们放寒假了~</s>）
-          </div>
-        </v-alert>
-
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.1</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/12/14 23:00</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>修复部分地图无法加载的问题</li>
-          <li>修复搜索中计时器固定显示“全天可钓”的问题</li>
-          <li>修复国服同步数据数量超过实际值的问题</li>
-          <li>更新6.0鱼的天气时间鱼饵信息</li>
-          <li>更新6.0刺鱼相关信息</li>
-        </ul>
-
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.1.0</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/12/06 22:20</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>
-            更新提钩列表，现在杆型以及精准、强力提钩显示在对应鱼的左侧，并将目标鱼加入列表
-          </li>
-          <li>
-            更新区服切换，点击左侧菜单栏中的切换按钮切换区服数据。切换“国际服”会显示6.X相关数据，请注意会包含剧透信息！
-          </li>
-          <li>更新列表中6.X相关数据，开荒中的数据会陆续更新。</li>
-          <li>更新鱼眼相关数据。</li>
-          <li>暂未更新：钓场范围提示，水族馆等数据。</li>
-        </ul>
-        <div class="text-h6 my-1">渔捞</div>
-        <ul>
-          <li>更新国际服6.0支持</li>
-          <li>刺鱼的自动记录功能暂未支持</li>
-          <li>更新对大尺寸以及Angler's Fortune的支持</li>
-        </ul>
-        <v-divider />
-
-        <v-alert color="secondary" outlined class="mt-4">
-          <div class="d-flex align-center">
-            <div class="text-h6">Version 1.0.5</div>
-            <v-spacer />
-            <div class="text-subtitle-1">更新时间: 2021/11/28 21:30</div>
-          </div>
-        </v-alert>
-        <div class="text-h6 my-1">鱼糕</div>
-        <ul>
-          <li>修正感谢名单排序错误</li>
-          <li>爱发电支持自选金额发电</li>
-          <li>
-            为所有根据以小钓大或鱼识前置鱼有所调整的鱼增加说明（鼠标悬停<v-icon>{{
-              mdiInformation
-            }}</v-icon
-            >查看）
-          </li>
-          <li>
-            增加海钓闹钟，在海钓航班处可以选择添加航班闹钟。闹钟列表与设置也增加了相应的内容
-          </li>
-          <li>
-            增加中庸工艺馆任务鱼的提示。感谢 <strong>凛田莉子@红玉海</strong> 的测试！
-          </li>
-          <li>更新部分鱼的闹钟提示文本，增加海钓闹钟的提示文本</li>
-          <li>调整各个页面切换简化模式的判定逻辑</li>
-          <li>调整显示钓场按钮的样式</li>
-          <li>调整鹦鹉螺、腔棘鱼、太阳海岸钓场银鲨的窗口期计算，现根据满月沙丁鱼显示</li>
-          <li>
-            增加部分任务鱼在列表中显示的可行鱼饵，显示低等级的鱼饵以适配职业任务时捕鱼人的实际等级
-          </li>
-          <li>修复“关于数据统计图”按钮失灵的问题</li>
-        </ul>
 
         <v-expansion-panels class="mt-4" flat>
           <v-expansion-panel>
@@ -329,7 +78,6 @@
 </template>
 
 <script>
-import { mdiInformation } from '@mdi/js'
 import RcDialog from '@/components/basic/RcDialog'
 
 export default {
@@ -353,9 +101,7 @@ export default {
     },
   },
   data() {
-    return {
-      mdiInformation,
-    }
+    return {}
   },
   methods: {
     onConfirmPatchNote() {
