6e1b0ce1ee56080c0b4e3b322ce86358cfe249ba
RiceCake404 <etenaelta@outlook.com>
2022-04-03 16:03:54 +0800

    fix(chart): fix echarts import LegendComponent

--- front/src/plugins/echarts.js
+++ front/src/plugins/echarts.js
@@ -3,14 +3,26 @@ import * as echarts from 'echarts/core'
 // 引入柱状图图表，图表后缀都为 Chart
 import { BarChart } from 'echarts/charts'
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
-import { GridComponent, TitleComponent, TooltipComponent } from 'echarts/components'
+import {
+  GridComponent,
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+} from 'echarts/components'
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
 import { CanvasRenderer } from 'echarts/renderers'
 
 import themes from '@/components/echart-theme/theme'
 
 // 注册必须的组件
-echarts.use([TitleComponent, TooltipComponent, GridComponent, BarChart, CanvasRenderer])
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  BarChart,
+  CanvasRenderer,
+])
 
 echarts.registerTheme('dark', themes.dark)
 echarts.registerTheme('light', themes.light)
